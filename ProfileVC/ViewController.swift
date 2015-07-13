eeebbb7fcd989b7630863c1f743b3ce2965fbf65
randy <randy@randydeMac-mini-3.local>
2015-07-13 11:46:14 +0800

    Initial Commit

---
+++ ProfileVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProfileVC
+//
+//  Created by randy on 15/7/13.
+//  Copyright (c) 2015年 randy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
